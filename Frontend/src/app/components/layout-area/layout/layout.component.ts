0ac38175f8dee7261e3e2f880000ba7ccee59696
yossi <a534158711@gmail.com>
2023-04-28 18:56:56 +0300

    sticky header cart and categories

--- Frontend/src/app/components/layout-area/layout/layout.component.ts
+++ Frontend/src/app/components/layout-area/layout/layout.component.ts
@@ -1,6 +1,7 @@
 import { CartService } from 'src/app/services/cart.service';
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/services/products.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-layout',
@@ -10,11 +11,14 @@ import { ProductsService } from 'src/app/services/products.service';
 export class LayoutComponent implements OnInit {
   constructor(
     private cartService : CartService,
-    private productsService : ProductsService
+    private productsService : ProductsService,
+    private auth: AuthService
   ){}
 
   async ngOnInit(): Promise<void> {
-
+    if(!this.auth.user){
+      return;
+    }
     try {
       await this.cartService.getCart();
       await this.productsService.getRandomProducts();

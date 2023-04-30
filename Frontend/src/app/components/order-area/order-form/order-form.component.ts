a5950ce8258b644e7c5ce8be6e382ce74606eb4f
yossi <a534158711@gmail.com>
2023-04-30 14:35:20 +0300

    order form builder

--- Frontend/src/app/components/order-area/order-form/order-form.component.ts
+++ Frontend/src/app/components/order-area/order-form/order-form.component.ts
@@ -1,17 +1,18 @@
 import { Router } from '@angular/router';
 import { CartService } from 'src/app/services/cart.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import User from 'src/app/models/auth-models/user.model';
 import Order from 'src/app/models/product-models/order.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { OrderService } from 'src/app/services/oreder.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-order-form',
   templateUrl: './order-form.component.html',
   styleUrls: ['./order-form.component.css']
 })
-export class OrderFormComponent {
+export class OrderFormComponent implements OnInit {
 
   public order : Order = new Order();
 
@@ -19,13 +20,28 @@ export class OrderFormComponent {
     private auth : AuthService,
     private orderService : OrderService,
     private cartService : CartService,
-    private router : Router
+    private router : Router,
+    private formBuilder : FormBuilder
   ) { }
 
+  async ngOnInit(): Promise<void> {
+    try {
+      await this.cartService.getCart();
+    } catch (error : any) {
+      alert(error.message);
+    }
+  }
+
+  public orderForm = this.formBuilder.group({
+    city: ['', [Validators.required, Validators.minLength(2)]],
+    street: ['', [Validators.required, Validators.minLength(2)]],
+    deliveryDate: [Date, [Validators.required]],
+    creditCard: ['', [Validators.required, Validators.pattern('([1-9]{4})')]]
+  })
   public copyDefaultUserDetails(cell: 'city' | 'street'){
     console.log(cell);
     
-    this.order[cell] = this.auth.user[cell]
+    this.orderForm.get(cell).setValue(this.auth.user[cell])
   }
 
   public async makeAnOrder(){

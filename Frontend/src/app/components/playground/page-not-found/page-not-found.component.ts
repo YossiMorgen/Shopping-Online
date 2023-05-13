cf826f580dcc0b5840a2df22840403209646c640
yossi <a534158711@gmail.com>
2023-05-13 21:49:36 +0300

    page not found return to products after 5 seconds

--- Frontend/src/app/components/playground/page-not-found/page-not-found.component.ts
+++ Frontend/src/app/components/playground/page-not-found/page-not-found.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css']
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit{
+  constructor(private router: Router) { }
 
+  ngOnInit(): void {
+    setTimeout(() => this.router.navigate(['products']), 5000);    
+  }
 }
